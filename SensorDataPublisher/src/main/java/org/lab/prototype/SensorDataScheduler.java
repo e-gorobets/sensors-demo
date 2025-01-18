10e7f0977c56d5f9810381448053ff06f4d8f544
RedQore <CSCGod6@gmail.com>
2025-01-19 00:34:03 +0400

    Added .env.sample and application.properties value to dynamically change fixedRate

--- SensorDataPublisher/src/main/java/org/lab/prototype/SensorDataScheduler.java
+++ SensorDataPublisher/src/main/java/org/lab/prototype/SensorDataScheduler.java
@@ -1,18 +1,19 @@
 package org.lab.prototype;
 
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+@PropertySource("classpath:application.properties")
 @Component
 public class SensorDataScheduler {
-
     private final SensorDataPublisher sensorDataPublisher;
 
     public SensorDataScheduler(SensorDataPublisher sensorDataPublisher) {
         this.sensorDataPublisher = sensorDataPublisher;
     }
 
-    @Scheduled(fixedRate = 5_000) // TODO: поменять частоту на каждые 10 минут (600_000)
+    @Scheduled(fixedRateString = "${fixed.rate}")
     public void publishTemperatureData() {
         sensorDataPublisher.publishTemperature();
     }
